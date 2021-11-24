46ccbe2f1fc06ab89586f5633d2fa1d988e5c112
Barry McCabe <92854061+bmac01@users.noreply.github.com>
2021-11-24 17:49:35 +0000

    Update of files

--- Student Grade/script.js
+++ Student Grade/script.js
@@ -1,31 +1,33 @@
 function Start(){	
 var numOfSubjects=parseInt(prompt("Please enter number of subjects"));
+var sum=0;
+var grades=0;
 
 	for(var i=0;i<numOfSubjects;i++){
-	sum=parseInt(prompt("Enter Mark For Subject"+" "+(i+1)));
-	  
-		if (sum/numOfSubjects>=70){
-			document.write("Average mark for all subjects: "+sum/numOfSubjects);
+		grades=sum+(parseInt(prompt("Enter Mark For Subject"+" "+(i+1))));
+		
+	  	if (grades/numOfSubjects>=70){
+			document.write("Average mark for all subjects: "+grades/numOfSubjects+"<br/>");
 			document.write("You received Grade A");
 
-		} else if (sum/numOfSubjects>=60){
-			document.write("Average mark for all subjects: "+sum/numOfSubjects);
+		} else if (grades/numOfSubjects>=60){
+			document.write("Average mark for all subjects: "+grades/numOfSubjects+"<br/>");
 			document.write("You received Grade B");
 
-		} else if (sum/numOfSubjects>=50){
-			document.write("Average mark for all subjects: "+sum/numOfSubjects);
+		} else if (grades/numOfSubjects>=50){
+			document.write("Average mark for all subjects: "+grades/numOfSubjects+"<br/>");
 			document.write("You received Grade C");
 
-		} else if (sum/numOfSubjects>=45){
-			document.write("Average mark for all subjects: "+sum/numOfSubjects);
+		} else if (grades/numOfSubjects>=45){
+			document.write("Average mark for all subjects: "+grades/numOfSubjects+"<br/>");
 			document.write("You received Grade D");
 
-		} else if (sum/numOfSubjects>=40){
-			document.write("Average mark for all subjects: "+sum/numOfSubjects);
+		} else if (grades/numOfSubjects>=40){
+			document.write("Average mark for all subjects: "+grades/numOfSubjects+"<br/>");
 			document.write("You received Grade E");
 
-		} else if (sum/numOfSubjects<=39){
-			document.write("Average mark for all subjects: "+sum/numOfSubjects);
+		} else if (grades/numOfSubjects<=39){
+			document.write("Average mark for all subjects: "+grades/numOfSubjects+"<br/>");
 			document.write("You received Grade F");
 		}
 
